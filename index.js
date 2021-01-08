26b76db670b0e9d09246bb69d04d8512f2d413e9
RYAN-Git-clone <ryanmaninga@gmail.com>
2021-01-09 01:00:08 +0300

    prevent default

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const getSecurity = document.querySelectorAll("button-one");
-getSecurity.addEventListener("click", function(event){
-    console.log(event);
-alert("You will be re-directed to a secure page");
-});
-
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+  });
